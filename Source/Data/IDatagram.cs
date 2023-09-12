9075c35700132740a79a81aeb7a526d9299e619f
xMakerx <maverick.liberty29@gmail.com>
2023-09-12 09:51:15 -0400

    SimClock: Add ticking, pinging, and pong functionality.

---
+++ Source/Data/IDatagram.cs
@@ -0,0 +1,21 @@
+﻿///////////////////////////////////////////////////////
+/// Filename: IDatagram.cs
+/// Date: September 12, 2023
+/// Author: Maverick Liberty
+///////////////////////////////////////////////////////
+
+namespace EppNet.Source.Data
+{
+
+    public interface IDatagram
+    {
+
+        public uint Header { internal set; get; }
+
+        public void Read();
+
+        public void WriteHeader();
+
+    }
+
+}
---
+++ Source/Simulation/ISimBase.cs
@@ -0,0 +1,21 @@
+﻿///////////////////////////////////////////////////////
+/// Filename: ISimBase.cs
+/// Date: September 12, 2023
+/// Author: Maverick Liberty
+///////////////////////////////////////////////////////
+
+namespace EppNet.Simulation
+{
+
+    public interface ISimBase
+    {
+        /// <summary>
+        /// Submits a request to synchronize clocks
+        /// </summary>
+        
+        public void RequestSync();
+        public SimClock GetClock();
+
+    }
+
+}
